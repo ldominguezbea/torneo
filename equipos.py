8af00e68c8a9ed638cc54f469ff6a4624874796d
nickk <ldominguezbea@alumno.huergo.edu.ar>
2026-08-19 11:01:31 -0300

    .

---
+++ equipos.py
@@ -0,0 +1,65 @@
+from random import *
+from tabulate import *
+
+equipos = []
+
+def crear_equipo():
+    global equipos
+    lista = []
+    cont = 0
+    cant = 0
+
+    while True:
+        try:
+            cant = int(input("Ingrese la cantidad de equipos: "))
+            break
+        except ValueError:
+            print("Error")
+        
+    while cont != cant:
+        equipo = input("Ingrese el nombre de su equipo:")
+        if equipo not in equipos:
+            equipos.append(equipo)
+            cont +=1
+        else:
+            print("El equipo ya esta en la lista")
+
+def partido():
+    rojo = "\033[91m"
+    verde = "\033[92m"
+    amarillo = "\033[93m"
+    celeste = "\033[96m"
+    fin = "\033[0m"
+
+    partidos = []
+    num = 0
+    for equipo in equipos:
+        random1 = choice(equipos)
+        random2 = choice(equipos)
+        gol1 = randint(0, 6)
+        gol2 = randint(0, 6)
+
+        if gol1 > gol2:
+            gol1 = verde + gol1 + fin
+            gol2 = rojo + gol2 + fin
+        elif gol1 < gol2:
+            gol2 = verde + gol2 + fin
+            gol1 = rojo + gol1 + fin
+        else:
+            gol2 = amarillo + gol2 + fin
+            gol1 = amarillo + gol1 + fin
+        num += 1
+
+        while random1 == random2:
+            random2 = choice(equipos)
+        tupla = tuple()
+        tupla = (num, ",", random1, ",", gol1, ",", random2, ",", gol2)
+
+        partidos.append(tupla)
+
+    print(tabulate(partidos, headers = ["Nº de partido", "Local", "Goles", "Visitante", "Goles", "Diferencia de puntos", "Goles a favor", "Puntos"], tablefmt = "grid"))
+
+
+
+    
+             
---
+++ main.py
@@ -0,0 +1,9 @@
+from equipos import *
+from tabulate import *
+
+def main():
+    print("===TORNEO===")
+    crear_equipo()
+main()
+
+partidos = partido()
