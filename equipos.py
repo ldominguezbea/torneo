87e8c6e072d1b8e83a860c140022f8609cba0ce1
ldominguezbea <ldominguezbea@alumno.huergo.edu.ar>
2026-08-25 16:41:35 -0300

    .

--- equipos.py
+++ equipos.py
@@ -1,113 +1,77 @@
-from random import *
-from tabulate import *
-from colorama import *
-from archivos import *
+from random import randint
+from tabulate import tabulate
+from colorama import init, Fore
+from archivos import guardar
 
-init (autoreset = True)
-def crear_equipo(): 
-    global dicc 
-    dicc = {}
-    
-    cont = 0
-    cant = 0
-
+init(autoreset=True)
+def crear_equipo():
+    dicc = []
     while True:
         try:
             cant = int(input("Ingrese la cantidad de equipos: "))
             break
         except ValueError:
             print("Error")
-        
+    cont = 0
     while cont != cant:
         equipo = input("Ingrese el nombre de su equipo:")
-        if equipo not in dicc:
-            dicc.append(
-                {
-                "equipo": equipo,
-                "pj": 0,
-                "pg": 0,
-                "pe": 0,
-                "pp": 0,
-                "gf": 0,
-                "gc": 0,
-                "dg": 0,
-                "pts": 0,
-            }
-        )
-            cont +=1
+        if equipo not in [e["EQUIPO"] for e in dicc]:
+            dicc.append({
+                "EQUIPO": equipo, "PJ": 0, "PG": 0, "PE": 0, "PP": 0,
+                "GF": 0, "GC": 0, "DG": 0, "PTS": 0,
+            })
+            cont += 1
         else:
             print("El equipo ya esta en la lista")
     return dicc
 
-def estadisticas(local, visitante):
 
+def estadisticas(local, visitante):
     gol_local = randint(0, 6)
     gol_visitante = randint(0, 6)
 
     local["PJ"] += 1
     visitante["PJ"] += 1
-
     local["GF"] += gol_local
     visitante["GF"] += gol_visitante
-
     local["GC"] += gol_visitante
     visitante["GC"] += gol_local
-
-    local["DG"] = gol_local - gol_visitante
-    visitante["DG"] = gol_visitante - gol_local
+    local["DG"] = local["GF"] - local["GC"]
+    visitante["DG"] = visitante["GF"] - visitante["GC"]
 
     if gol_local > gol_visitante:
-        local["PTOS"] += 3
+        local["PTS"] += 3
         local["PG"] += 1
-        print(local, Fore.GREEN +"ganó el partido")
         visitante["PP"] += 1
+        print(local["EQUIPO"], Fore.GREEN + "ganó el partido")
+
     elif gol_visitante > gol_local:
-        visitante["PTOS"] += 3
+        visitante["PTS"] += 3
         visitante["PG"] += 1
         local["PP"] += 1
-        print(visitante, Fore.GREEN + "ganó el partido")
+        print(visitante["EQUIPO"], Fore.GREEN + "ganó el partido")
     else:
-        visitante["PTOS"] += 1
-        local["PTOS"] = 1
+        local["PTS"] += 1
+        visitante["PTS"] += 1
+        local["PE"] += 1
         visitante["PE"] += 1
-        local["PE"] = 1
         print(Fore.YELLOW + "Empate")
-    guardar(dicc, "equipos.json")
 
 def fixture(dicc):
-    campeon =  {
-                "EQUIPO": "equipo",
-                "PJ": 0,
-                "PG": 0,
-                "PE": 0,
-                "PP": 0,
-                "GF": 0,
-                "GC": 0,
-                "DG": 0,
-                "PTS": 0,
-            }
     cant_equipos = len(dicc)
-    for i in range():
-        for x in range(i +1, cant_equipos):
+    for i in range(cant_equipos):
+        for x in range(i + 1, cant_equipos):
             estadisticas(dicc[i], dicc[x])
-    for clave, valor in dicc.items():
-        if dicc[clave]["PJ"]["PG"] > campeon["EQUIPO"]["PJ"]["PG"]:
-            campeon[clave]
 
-    mostrar_fixture()
-            
-def mostrar_fixture():
-    print(tabulate(estadisticas, headers = ["Nº de partido", 
-    "Local", 
-    "Goles", 
-    "Puntos", 
-    "Diferencia de puntos", 
-    "Goles a favor", 
-    "Goles en contra"
-    "Visitante", 
-    "Goles", 
-    "Puntos", 
-    "Diferencia de puntos", 
-    "Goles a favor", 
-    "Goles en contra"], tablefmt = "grid"))
-    
+    guardar(dicc, "equipos.json")
+    campeon = dicc[0]
+    for equipo in dicc:
+        if equipo["PTS"] > campeon["PTS"]:
+            campeon = equipo
+
+    mostrar_fixture(dicc)
+    print(Fore.CYAN + "Campeón:", campeon["EQUIPO"], "con", campeon["PTS"], "puntos")
+
+def mostrar_fixture(dicc):
+    filas = [[e["EQUIPO"], e["PJ"], e["PG"], e["PE"], e["PP"], e["GF"], e["GC"], e["DG"], e["PTS"]] for e in dicc]
+    print(tabulate(filas, headers=["Equipo", "PJ", "PG", "PE", "PP", "GF", "GC", "DG", "PTS"], tablefmt="grid"))
